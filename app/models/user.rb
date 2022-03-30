e97981a0ecca07e10ba619d51c26d3fb4e31859f
GuiMarcelino <guilhermemarcelino_91@outlook.com>
2022-03-30 06:03:29 -0300

    fix/refactor table users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,66 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
+
+
+  # Callbacks
+  before_validation :set_provider,
+                    :set_uid
+
+  # Validations
+  validates_presence_of :email, {
+    message: :need_email
+  }
+  validates_uniqueness_of :email,
+                          allow_blank: true,
+                          conditions: -> { where(active: true, deleted_at: nil) }
+  validates_format_of :email,
+                      with: /\A[^@\s]+@[^@\s]+\z/,
+                      allow_blank: true,
+                      if: :email_changed?
+
+  validates_uniqueness_of :uid,
+                          scope: :provider,
+                          conditions: -> { where(active: true, deleted_at: nil) }
+
+  validates_presence_of :password, if: :password_required?
+  validates_length_of :password, within: 6..128, allow_blank: true
+  validates_confirmation_of :password, if: :password_required?
+  validates_format_of :password,
+                      with: /(?=.*[a-z])(?=.*[A-Z])/,
+                      message: :invalid_case_format,
+                      if: -> { password.present? }
+  validates_format_of :password,
+                      with: /(?=.*[0-9])/,
+                      message: :invalid_number_format,
+                      if: -> { password.present? }
+  validates_format_of :password,
+                      with: /(?=.*[^A-Za-z0-9])/,
+                      message: :invalid_special_character,
+                      if: -> { password.present? }
+
+
+  validates :name, presence: true, length: { maximum: 255 }
+
+
+  protected
+
+  def password_required?
+    !persisted? || !password.nil? || !password_confirmation.nil?
+  end
+
+  private
+
+  def set_provider
+    self.provider = 'email'
+  end
+
+  def set_uid
+    self.uid = email
+  end
+
+
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -16,11 +16,16 @@ ActiveRecord::Schema.define(version: 2022_03_29_235828) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
+    t.string "name"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "provider", null: false
+    t.string "uid", default: "", null: false
+    t.boolean "active", default: true, null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.datetime "deleted_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
